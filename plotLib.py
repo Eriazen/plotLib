83255769fe872c4617ac555604d51140cd581945
eriazen <vita.vecernik@seznam.cz>
2024-10-06 13:29:54 +0200

    Code cleanup

--- plotLib.py
+++ plotLib.py
@@ -1,37 +1,38 @@
+from __future__ import annotations
 import pandas as pd
 
 class data:
     """ python class to load data from table files """
 
-    def __init__(self,dirs):
+    def __init__(self, dirs: str) -> data:
         self.dirs = dirs
+        self.tables = []
     
-    def loadData(self, file, delim):
+    def loadData(self, file: str, delim: str) -> None:
         """ loads data into pandas DataFrame """
 
-        self.tables = {}
-        i = 0
         for dir in self.dirs:
-            self.tables[i] = pd.read_table(dir+file,delimiter=delim)
-            i += 1
+            self.tables.append(pd.read_table(dir+file,delimiter=delim))
 
-    def selectData(self, col, arg, sort):
+    def selectData(self, col: str, sort: str, argList: list) -> None:
         """ selects data based on values in a specified column,
-         if arg is None, only select column"""
+         \nif arg isn't specified, only select column """
 
-        for i in range(0,self.tables.__len__()):
-            if arg == None:
-                self.tables[i] = self.tables[i][col].sort_values()
+        for id, tbl in enumerate(self.tables):
+            if argList == None:
+                self.tables[id] = tbl[col].sort_values()
             else:
-                self.tables[i] = self.tables[i].loc[self.tables[i][col] == arg].sort_values(by=[sort])
+                self.tables[id] = tbl.loc[tbl[col] == argList[id]].sort_values(by=sort)
 
-    def assignValues(self, varList):
-        """ assigns values into lists in a dictionary """
+    def assignValues(self, varList: list) -> list:
+        """ assigns values into lists in a list """
 
-        values = {}
-        for i in range(0,self.tables.__len__()):
+        values = []
+
+        for tbl in self.tables:
             vars = []
             for var in varList:
-                vars.append(self.tables[i][var])
-            values[i] = vars
+                vars.append(tbl[var])
+            values.append(vars)
+
         return values
